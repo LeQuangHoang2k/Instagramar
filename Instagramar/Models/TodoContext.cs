b0f5a022784d67d3c2c2d37b53a81c1176c51077
LeQuangHoang2k <60897161+LeQuangHoang2k@users.noreply.github.com>
2024-01-27 11:15:18 +0700

    add controller + model Users

--- Instagramar/Models/TodoContext.cs
+++ Instagramar/Models/TodoContext.cs
@@ -12,6 +12,20 @@
 //     public DbSet<TodoItem> TodoItems { get; set; } = null!;
 // }
 
+//using Microsoft.EntityFrameworkCore;
+
+//namespace Instagramar.Models;
+
+//public class TodoContext : DbContext
+//{
+//    public TodoContext(DbContextOptions<TodoContext> options)
+//        : base(options)
+//    {
+//    }
+
+//    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+//}
+
 using Microsoft.EntityFrameworkCore;
 
 namespace Instagramar.Models;
@@ -24,4 +38,5 @@ public class TodoContext : DbContext
     }
 
     public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
 }
---
+++ Instagramar/Models/User.cs
@@ -0,0 +1,18 @@
+﻿//namespace Instagramar.Models
+//{
+//    public class User
+//    {
+//    }
+//}
+
+namespace Instagramar.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string Username { get; set; }
+    public string HashPassword { get; set; }
+    public string? Name { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+}
