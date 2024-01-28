ffcd1ce708ff8bb7ffabc8b6f9a0fa4202be08c0
LeQuangHoang2k <60897161+LeQuangHoang2k@users.noreply.github.com>
2024-01-28 11:34:59 +0700

    ad signin signup forgot

---
+++ Instagramar/Models/Request/User/SignIn.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Instagramar.Models.Request.User
+{
+    public class SignIn
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Book name is required")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Password { get; set; }
+    }
+}
